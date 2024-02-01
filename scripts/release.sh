cff137009e920b00762e49929498b81049ae2ec8
Abdullah Ahmed <12223088+geektype@users.noreply.github.com>
2024-02-01 17:19:41 +0000

    update

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,8 @@
-echo $GITHUB_TOKEN
+AUTH="Authorization:Bearer ${GITHUB_TOKEN}"
 
-URL="https://api.github.com/repos/${OWNER}/${REPO}/releases/${RELEASE}"
-HEADERS="Authorization:Bearer ${GITHUB_TOKEN}"
-DATA="{\"body\": \"yeetus bafeetus\"}"
-
-curl -s -X PATCH -H "${HEADERS}" -H "Content-Type: application/json" -d "${DATA}" "${URL}"
+curl -L \
+    -X POST \
+    -H "${AUTH}" \
+    -H "Content-Type: application/octet-stream" \
+    "https://uploads.github.com/repos/${OWNER}/${REPO}/releases/${RELEASE}/assets?name=shelly-${PLATFORM}" \
+    --data-binary "@../target/release/shelly"
